adf856811b6e6ac488c9832153417f062aa20486
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-13 17:05:17 +0100

    Update node.cpp

--- robot_ws/src/camera_ingest/ros_node/src/node.cpp
+++ robot_ws/src/camera_ingest/ros_node/src/node.cpp
@@ -7,7 +7,7 @@ NSSC::NSSC() : Node("NSSC")
 
 void NSSC::printInfo(std::string caller, std::string msg)
 {
-    boost::format fmter("[%1%] %2%");
+    boost::format fmter("\033[1;34m[%1%] \033[0m%2%");
     fmter % caller; fmter % msg;
     RCLCPP_INFO(this->get_logger(), fmter.str());
 }
