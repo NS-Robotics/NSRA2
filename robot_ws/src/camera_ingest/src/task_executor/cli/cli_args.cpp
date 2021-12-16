c6568af3b1ad58660f14dba11b464d2b3e4ec4ce
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-16 08:11:19 +0100

    Update cli_args.cpp

--- robot_ws/src/camera_ingest/src/task_executor/cli/cli_args.cpp
+++ robot_ws/src/camera_ingest/src/task_executor/cli/cli_args.cpp
@@ -68,6 +68,7 @@ NSSC_STATUS CLI::getStrArg(std::vector<char*> cmd, char par, char** ret)
             strcpy(*ret, cmd[i]);
 
             std::cout << *ret << std::endl;
+            std::cout << **ret[strlen(*ret) - 1] << std::endl;
             
             while (*ret[strlen(*ret) - 1] != '"' && strlen(*ret) > 0)
                 *ret[strlen(*ret) - 1] = '\0';
