65a153cf14d80862f19e7367268089b5cc337860
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-18 19:53:51 +0100

    Update cli.cpp

--- robot_ws/src/camera_ingest/src/task_executor/cli/cli.cpp
+++ robot_ws/src/camera_ingest/src/task_executor/cli/cli.cpp
@@ -71,8 +71,7 @@ void CLI::CLIFunc()
                    "  ingest [-n number of images -d set name]  - Start the calibration capture\n"
                    "  calibrate [-d image set name]             - Run the calibration\n"
                    "  run [-c calibration config]               - Run NSSC\n"
-                   "  exit                                      - Close the application\n"
-                   "  test")
+                   "  exit                                      - Close the application\n");
         }
         else
         {
