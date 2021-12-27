5fcfb72adafe98c9b5b5874a04f906f4c92afbea
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-27 19:01:39 +0100

    Update ingest.cpp

--- robot_ws/src/camera_ingest/src/ingest/ingest.cpp
+++ robot_ws/src/camera_ingest/src/ingest/ingest.cpp
@@ -8,7 +8,7 @@ Ingest::Ingest(std::shared_ptr<NSSC>& node, std::shared_ptr<cameraManager>& camM
     this->setName = setName;
 
     this->setPath = this->node->g_config.share_dir + "/NSSC/" + setName + "/";
-    int status = std::system("mkdir -p " + this->setPath);
+    int status = std::system(("mkdir -p " + this->setPath).c_str());
     if (status == -1)
         this->node->printInfo(this->msgCaller, "Not created!");
     else
