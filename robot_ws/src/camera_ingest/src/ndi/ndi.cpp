4ce70dc058928c6b098355080ab4a85fb97a8dd3
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-28 00:18:55 +0100

    Update ndi.cpp

--- robot_ws/src/camera_ingest/src/ndi/ndi.cpp
+++ robot_ws/src/camera_ingest/src/ndi/ndi.cpp
@@ -106,7 +106,7 @@ void NDI::streamThread()
         {
             cv::Mat sendFrame(cv::Size(this->node->g_config.frameConfig.stream_x_res, this->node->g_config.frameConfig.stream_y_res), CV_8UC4, stereoFrame->stereoBuf.hImageBuf);
 
-            cv::putText(sendFrame, std::to_string(this->node->g_config.ingestConfig.current_frame_idx), cv::Point(25, 60), //top-left position
+            cv::putText(sendFrame, "Image idx: " + std::to_string(this->node->g_config.ingestConfig.current_frame_idx), cv::Point(25, 60), //top-left position
                         cv::FONT_HERSHEY_DUPLEX,
                         2.0,
                         cv::Scalar(254, 0, 0), //font color
@@ -115,7 +115,7 @@ void NDI::streamThread()
             auto now = std::chrono::high_resolution_clock::now();
             auto time_left = std::chrono::duration_cast<std::chrono::milliseconds>(now - this->node->g_config.ingestConfig.sleep_timestamp);
 
-            cv::putText(sendFrame, std::to_string(5000 - time_left.count()), cv::Point(100, 60), //top-left position
+            cv::putText(sendFrame, "Next image in " + std::to_string(5000 - time_left.count()) + " ms", cv::Point(200, 60), //top-left position
                         cv::FONT_HERSHEY_DUPLEX,
                         2.0,
                         cv::Scalar(254, 0, 0), //font color
