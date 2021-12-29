312a759f6f5ea02d732f8a352c8ff47a0c78d22a
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-29 18:33:30 +0100

    Update camera_manager.cpp

--- robot_ws/src/camera_ingest/src/camera_manager/camera_manager.cpp
+++ robot_ws/src/camera_ingest/src/camera_manager/camera_manager.cpp
@@ -69,7 +69,7 @@ NSSC_STATUS cameraManager::closeCameras()
     }
 }
 
-stereoFrame *cameraManager::getFrame(bool concatenate = false, bool resize = false)
+stereoFrame *cameraManager::getFrame(bool concatenate /*= false*/, bool resize /*= false*/)
 {
     stereoFrame* stereoFrame;
     this->emptyFrameBuf.wait_dequeue(stereoFrame);
