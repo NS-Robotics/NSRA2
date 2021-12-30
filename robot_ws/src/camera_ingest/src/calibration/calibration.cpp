b018a486b2517b0ad1b5f03e4243ee83ae1e47a7
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2021-12-31 00:30:25 +0100

    Update calibration.cpp

--- robot_ws/src/camera_ingest/src/calibration/calibration.cpp
+++ robot_ws/src/camera_ingest/src/calibration/calibration.cpp
@@ -42,7 +42,7 @@ void Calibration::_prepareDataSet()
 
     for (int i = 0; i < this->num_images; i++)
     {
-        this->ioService.post(boost::bind(__CBCthreadTask, i));
+        this->ioService.post(boost::bind(__CBCthreadTask, i);
     }
 
     this->ioService.stop();
