24fac891b21d49fbf95ab87ccb70acb483b73af6
Noothless <noa.sendlhofer@gmail.com>
2022-02-05 12:01:53 +0000

    robot_calibration

---
+++ robot_ws/src/robot_calibration/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'robot_calibration'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nsra_control',
+    maintainer_email='noa.sendlhofer@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
