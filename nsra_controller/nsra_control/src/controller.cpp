7e9e71611707d5cc39c7fdf170b922db64ca8464
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-02-05 01:34:16 +0100

    Update controller.cpp

--- nsra_controller/nsra_control/src/controller.cpp
+++ nsra_controller/nsra_control/src/controller.cpp
@@ -51,12 +51,12 @@
 #define VEL_LIMIT_A5 3.0
 #define VEL_LIMIT_A6 3.0
 
-#define VEL_DIFF_A1 300.0
-#define VEL_DIFF_A2 200.0
-#define VEL_DIFF_A3 100.0
-#define VEL_DIFF_A4 100.0
-#define VEL_DIFF_A5 100.0
-#define VEL_DIFF_A6 100.0
+#define VEL_DIFF_A1 3.0
+#define VEL_DIFF_A2 2.0
+#define VEL_DIFF_A3 1.0
+#define VEL_DIFF_A4 1.0
+#define VEL_DIFF_A5 1.0
+#define VEL_DIFF_A6 1.0
 
 #define GRIPPER_PIN 2
 
@@ -93,9 +93,9 @@ ArduinoQueue<pos> queue(50);
 
 pos last;
 
-float calc_vel(float last, float now, float next)
+float calc_vel(float last, float now, float next, float diff)
 {
-  return (abs(last - now) + abs(now - next)) / 2.0 * (float)FRQ;
+  return (abs(last - now) + abs(now - next)) / 2.0 * (float)FRQ + diff;
 }
 
 void update() {
@@ -105,37 +105,37 @@ void update() {
     pos now = queue.dequeue();
     pos next = queue.getHead();
 
-    float axis1_vel = calc_vel(last.axis1, now.axis1, next.axis1);
+    float axis1_vel = calc_vel(last.axis1, now.axis1, next.axis1, VEL_DIFF_A1);
     if(axis1_vel < VEL_LIMIT_A1)
       odrive_serial1 << "w axis" << 0 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A1 << '\n';
     else
       odrive_serial1 << "w axis" << 0 << ".controller.config.vel_limit " << axis1_vel << '\n';
 
-    float axis2_vel = calc_vel(last.axis2, now.axis2, next.axis2);
+    float axis2_vel = calc_vel(last.axis2, now.axis2, next.axis2, VEL_DIFF_A2);
     if (axis2_vel < VEL_LIMIT_A2)
       odrive_serial0 << "w axis" << 0 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A2 << '\n';
     else
       odrive_serial0 << "w axis" << 0 << ".controller.config.vel_limit " << axis2_vel << '\n';
 
-    float axis3_vel = calc_vel(last.axis3, now.axis3, next.axis3);
+    float axis3_vel = calc_vel(last.axis3, now.axis3, next.axis3, VEL_DIFF_A3);
     if (axis3_vel < VEL_LIMIT_A3)
       odrive_serial0 << "w axis" << 1 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A3 << '\n';
     else
       odrive_serial0 << "w axis" << 1 << ".controller.config.vel_limit " << axis3_vel << '\n';
 
-    float axis4_vel = calc_vel(last.axis4, now.axis4, next.axis4);
+    float axis4_vel = calc_vel(last.axis4, now.axis4, next.axis4, VEL_DIFF_A4);
     if (axis4_vel < VEL_LIMIT_A4)
       odrive_serial2 << "w axis" << 0 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A4 << '\n';
     else
       odrive_serial2 << "w axis" << 0 << ".controller.config.vel_limit " << axis4_vel << '\n';
 
-    float axis5_vel = calc_vel(last.axis5, now.axis5, next.axis5);
+    float axis5_vel = calc_vel(last.axis5, now.axis5, next.axis5, VEL_DIFF_A5);
     if (axis5_vel < VEL_LIMIT_A5)
       odrive_serial2 << "w axis" << 1 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A5 << '\n';
     else
       odrive_serial2 << "w axis" << 1 << ".controller.config.vel_limit " << axis5_vel << '\n';
 
-    float axis6_vel = calc_vel(last.axis6, now.axis6, next.axis6);
+    float axis6_vel = calc_vel(last.axis6, now.axis6, next.axis6, VEL_DIFF_A6);
     if (axis6_vel < VEL_LIMIT_A6)
       odrive_serial1 << "w axis" << 1 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A6 << '\n';
     else
