9f6806427dd8ccceb1e49ad698142ad0d3485615
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-02-07 12:41:19 +0100

    moveit packages

---
+++ robot_ws/src/robot_interface/nsra2/src/nsra2_interface.cpp
@@ -0,0 +1,27 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/msg/display_robot_state.hpp>
+#include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <moveit_msgs/msg/attached_collision_object.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    auto move_group_node = rclcpp::Node::make_shared("nsra2_interface", node_options);
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(move_group_node);
+    std::thread([&executor]()
+                { executor.spin(); }).detach();
+
+}
