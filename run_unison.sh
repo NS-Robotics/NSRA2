e9870933878c1cdc62646577ce59572a8902696c
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-02-15 13:53:43 +0100

    Update run_unison.sh

--- run_unison.sh
+++ run_unison.sh
@@ -4,8 +4,11 @@ while true;
 do
 unison -batch -auto -silent nsra_management.prf
 unison-2.48 -batch -auto -silent nsra_control.prf
-unison-2.48 -batch -auto -silent nsra_viewer.prf
+#unison-2.48 -batch -auto -silent nsra_viewer.prf
+unison-2.48 -batch -auto -silent rpi4_desktop.prf
 #unison-2.48 -batch -auto -silent nssc.prf
 
+now="$(date +"%T")"
+echo "Workspace synchronized! $now"
 sleep 5s
 done
