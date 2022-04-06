7018a4ac0f89de9b6bef73337f2e083e0dcc8a49
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-04-06 18:07:18 +0200

    Update ingest.cpp

--- robot_ws/src/camera_ingest/src/calib_cameras/ingest/ingest.cpp
+++ robot_ws/src/camera_ingest/src/calib_cameras/ingest/ingest.cpp
@@ -124,7 +124,7 @@ void nssc::stereocalibration::Ingest::ingestThread()
     while (this->run_ingest.load() &&
             this->node->g_config.ingestConfig.current_frame_idx < this->node->g_config.ingestConfig.ingest_amount)
     {
-        std::chrono::milliseconds difference = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - this->node->g_config.ingestConfig.sleep_timestamp).count();
+        std::chrono::milliseconds difference = std::chrono::high_resolution_clock::now() - this->node->g_config.ingestConfig.sleep_timestamp;
         std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(this->node->g_config.ingestConfig.wait_duration);
         if (difference >= duration)
         {
