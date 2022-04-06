6f922164e4f7e7205930a87d4b6c69fbe45cb84b
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-04-07 00:12:51 +0200

    web msg

--- robot_ws/src/nssc_web_interface/nssc_web_interface/nssc_web_interface.py
+++ robot_ws/src/nssc_web_interface/nssc_web_interface/nssc_web_interface.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask, render_template, request
 from ament_index_python.packages import get_package_share_directory
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from camera_ingest.msg import ColorFilterParams
 
 package_share_directory = get_package_share_directory('nssc_web_interface')
 print(package_share_directory)
@@ -18,6 +22,19 @@ def test():
     lower_u = request.form["lower_u"]
     lower_v = request.form["lower_v"]
     return str(lower_y) + " - " + str(lower_u) + " - " + str(lower_v)
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(ColorFilterParams, 'topic', 10)
+
+    def timer_callback(self):
+        msg = ColorFilterParams()
+        msg.low_H = 10
+        #self.publisher_.publish(msg)
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
+        #self.i += 1
 
 def main():
     app.run(host="0.0.0.0")
