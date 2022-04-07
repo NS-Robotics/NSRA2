4996033682554ae6e67742819d5f502c060e38e8
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-04-07 09:39:03 +0200

    keep value

--- robot_ws/src/nssc_web_interface/nssc_web_interface/nssc_web_interface.py
+++ robot_ws/src/nssc_web_interface/nssc_web_interface/nssc_web_interface.py
@@ -6,6 +6,7 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from camera_ingest.msg import ColorFilterParams
 import threading
+from dataclasses import dataclass
 
 package_share_directory = get_package_share_directory('nssc_web_interface')
 print(package_share_directory)
@@ -15,41 +16,59 @@ app = Flask(__name__, template_folder=template_dir)
 
 color_filter_params = None
 
+@dataclass
+class FilterStruct:
+    low_h: int
+    low_s: int
+    low_v: int
+    high_h: int
+    high_s: int
+    high_v: int
+
+current_pos = FilterStruct(50, 128, 128, 128, 128, 128)
+
 class WebCommandPublisher(Node):
 
     def __init__(self):
         super().__init__('nssc_web_interface')
         self.publisher_ = self.create_publisher(ColorFilterParams, 'color_filter_params', 10)
 
-    def sendCommand(self, low_h, low_s, low_v, high_h, high_s, high_v):
+    def sendCommand(self, current_pos):
         msg = ColorFilterParams()
-        msg.low_h = low_h
-        msg.low_s = low_s
-        msg.low_v = low_v
-        msg.high_h = high_h
-        msg.high_s = high_s
-        msg.high_v = high_v
+        msg.low_h = current_pos.low_h
+        msg.low_s = current_pos.low_s
+        msg.low_v = current_pos.low_v
+        msg.high_h = current_pos.high_h
+        msg.high_s = current_pos.high_s
+        msg.high_v = current_pos.high_v
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing command')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', low_h = current_pos.low_h)
 
-@app.route("/test", methods=["POST"])
+@app.route("/test", methods=["POST", "GET"])
 def test():
     global color_filter_params
+    global current_pos
 
-    low_h = request.form["low_h"]
-    low_s = request.form["low_s"]
-    low_v = request.form["low_v"]
-    high_h = request.form["high_h"]
-    high_s = request.form["high_s"]
-    high_v = request.form["high_v"]
+    if request.method == 'POST':
 
-    color_filter_params.sendCommand(int(low_h), int(low_s), int(low_v), int(high_h), int(high_s), int(high_v))
+        current_pos.low_h = int(request.form["low_h"])
+        current_pos.low_s = int(request.form["low_s"])
+        current_pos.low_v = int(request.form["low_v"])
+        current_pos.high_h = int(request.form["high_h"])
+        current_pos.high_s = int(request.form["high_s"])
+        current_pos.high_v = int(request.form["high_v"])
 
-    return redirect('/')
+        color_filter_params.sendCommand(current_pos)
+
+        return redirect('/')
+
+    else:
+        return render_template('index.html', low_h = current_pos.low_h)
+
 
 def run_page():
     app.run(host="0.0.0.0")
