86dbd504ed268fa951c61b010924bea6faff5a43
Noa Sendlhofer <noa.sendlhofer@gmail.com>
2022-05-22 20:23:29 +0200

    Update controller.cpp

--- nsra_controller/nsra_control/src/controller.cpp
+++ nsra_controller/nsra_control/src/controller.cpp
@@ -1,34 +1,34 @@
 /*****************************************************************************
-*  BSD 3-Clause License
-* 
-*  Copyright (c) 2020, Noa Sendlhofer
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions are met:
-* 
-*  1. Redistributions of source code must retain the above copyright notice, this
-*     list of conditions and the following disclaimer.
-* 
-*  2. Redistributions in binary form must reproduce the above copyright notice,
-*     this list of conditions and the following disclaimer in the documentation
-*     and/or other materials provided with the distribution.
-* 
-*  3. Neither the name of the copyright holder nor the names of its
-*     contributors may be used to endorse or promote products derived from
-*     this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-*  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-*  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-*  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-*  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-*  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-****************************************************************************/
+ *  BSD 3-Clause License
+ *
+ *  Copyright (c) 2020, Noa Sendlhofer
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *  3. Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ****************************************************************************/
 
 /* Author: Noa Sendlhofer
    Desc:   Part of the NSRA Control stack. Teensy ODrive UART controller.
@@ -68,12 +68,22 @@
 
 #define GRIPPER_PIN 2
 
-template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
-template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
+template <class T>
+inline Print &operator<<(Print &obj, T arg)
+{
+  obj.print(arg);
+  return obj;
+}
+template <>
+inline Print &operator<<(Print &obj, float arg)
+{
+  obj.print(arg, 4);
+  return obj;
+}
 
-HardwareSerial& odrive_serial0 = Serial3;
-HardwareSerial& odrive_serial1 = Serial2;
-HardwareSerial& odrive_serial2 = Serial1;
+HardwareSerial &odrive_serial0 = Serial3;
+HardwareSerial &odrive_serial1 = Serial2;
+HardwareSerial &odrive_serial2 = Serial1;
 
 ODriveArduino odrv0(odrive_serial0);
 ODriveArduino odrv1(odrive_serial1);
@@ -88,14 +98,15 @@ bool controlFlag = false;
 
 volatile bool gripper_enabled = false;
 
-struct pos {
-    volatile float axis1;
-    volatile float axis2;
-    volatile float axis3;
-    volatile float axis4;
-    volatile float axis5;
-    volatile float axis6;
-    pos(): axis1(0), axis2(0), axis3(0), axis4(0), axis5(0), axis6(0) {}
+struct pos
+{
+  volatile float axis1;
+  volatile float axis2;
+  volatile float axis3;
+  volatile float axis4;
+  volatile float axis5;
+  volatile float axis6;
+  pos() : axis1(0), axis2(0), axis3(0), axis4(0), axis5(0), axis6(0) {}
 };
 ArduinoQueue<pos> queue(50);
 
@@ -106,15 +117,15 @@ float calc_vel(float last, float now, float next, float diff)
   return (abs(last - now) + abs(now - next)) / 2.0 * (float)FRQ + diff;
 }
 
-void update() {
-  if(queueFlag && controlFlag)
+void update()
+{
+  if (queueFlag && controlFlag)
   {
-    
     pos now = queue.dequeue();
     pos next = queue.getHead();
 
     float axis1_vel = calc_vel(last.axis1, now.axis1, next.axis1, VEL_DIFF_A1);
-    if(axis1_vel < VEL_LIMIT_A1)
+    if (axis1_vel < VEL_LIMIT_A1)
       odrive_serial1 << "w axis" << 0 << ".controller.config.vel_limit " << (float)VEL_LIMIT_A1 << '\n';
     else
       odrive_serial1 << "w axis" << 0 << ".controller.config.vel_limit " << axis1_vel << '\n';
@@ -157,26 +168,31 @@ void update() {
     odrv1.SetPosition(1, (float)now.axis6);
 
     last = now;
-    
-  } else if(queue.itemCount() >= QUEUE_SIZE && !queueFlag){
+  }
+  else if (queue.itemCount() >= QUEUE_SIZE && !queueFlag)
+  {
     queueFlag = true;
   }
-  
-  if(queue.itemCount() == 0 && queueFlag){
+
+  if (queue.itemCount() == 0 && queueFlag)
+  {
     queueFlag = false;
   }
-  
 }
 
-void serial_interrupt() {
+void serial_interrupt()
+{
 
   char in_bytes[24];
   CRC32 crc;
-  
+
   while (Serial.available())
   {
     in_bytes[0] = Serial.read();
-    if (in_bytes[0] == 10) { break; }
+    if (in_bytes[0] == 10)
+    {
+      break;
+    }
   }
 
   for (int n = 0; n < 24; n++)
@@ -200,28 +216,32 @@ void serial_interrupt() {
   rec_checksum = rec_checksum << 8;
   rec_checksum = rec_checksum | dec_string[13];
 
-  if(rec_checksum == checksum) {
+  if (rec_checksum == checksum)
+  {
 
-    if(!controlFlag) {
+    if (!controlFlag)
+    {
       serialFlag = true;
       digitalWrite(LED_BUILTIN, HIGH);
     }
-    
+
     pos n;
 
-    n.axis1 = (float)((uint16_t)((dec_string[1] << 8) | dec_string[0]) - 32000)   / multiplier * TRANSMISSION_A1;
-    n.axis2 = (float)((uint16_t)((dec_string[3] << 8) | dec_string[2]) - 32000)   / multiplier * TRANSMISSION_A2;
-    n.axis3 = (float)((uint16_t)((dec_string[5] << 8) | dec_string[4]) - 32000)   / multiplier * TRANSMISSION_A3;
-    n.axis4 = (float)((uint16_t)((dec_string[7] << 8) | dec_string[6]) - 32000)   / multiplier * TRANSMISSION_A4;
-    n.axis5 = (float)((uint16_t)((dec_string[9] << 8) | dec_string[8]) - 32000)   / multiplier * TRANSMISSION_A5;
+    n.axis1 = (float)((uint16_t)((dec_string[1] << 8) | dec_string[0]) - 32000) / multiplier * TRANSMISSION_A1;
+    n.axis2 = (float)((uint16_t)((dec_string[3] << 8) | dec_string[2]) - 32000) / multiplier * TRANSMISSION_A2;
+    n.axis3 = (float)((uint16_t)((dec_string[5] << 8) | dec_string[4]) - 32000) / multiplier * TRANSMISSION_A3;
+    n.axis4 = (float)((uint16_t)((dec_string[7] << 8) | dec_string[6]) - 32000) / multiplier * TRANSMISSION_A4;
+    n.axis5 = (float)((uint16_t)((dec_string[9] << 8) | dec_string[8]) - 32000) / multiplier * TRANSMISSION_A5;
     n.axis6 = (float)((uint16_t)((dec_string[11] << 8) | dec_string[10]) - 32000) / multiplier * TRANSMISSION_A6;
     gripper_enabled = (uint8_t)dec_string[12];
-  
+
     queue.enqueue(n);
+  }
+  else if (controlFlag)
+  {
 
-  } else if(controlFlag) {
-
-    while(queue.itemCount() != 0) {
+    while (queue.itemCount() != 0)
+    {
       queue.dequeue();
     }
 
@@ -229,10 +249,10 @@ void serial_interrupt() {
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(GRIPPER_PIN, LOW);
   }
-
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(912600);
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -245,32 +265,38 @@ void setup() {
 
   delay(2000);
 
-  while (!Serial) {
+  while (!Serial)
+  {
     delay(1);
   }
 
-  ctrl_loop_timer.begin(update, 1000000/FRQ);
-
+  ctrl_loop_timer.begin(update, 1000000 / FRQ);
 }
 
-void loop() {
-  
-  if(Serial.available() > 24) {
+void loop()
+{
+
+  if (Serial.available() > 24)
+  {
     serial_interrupt();
   }
 
-  if(serialFlag) {
+  if (serialFlag)
+  {
     serialFlag = false;
     controlFlag = true;
   }
 
-  if(controlFlag) {
+  if (controlFlag)
+  {
     digitalWrite(GRIPPER_PIN, gripper_enabled);
   }
 
-  if(!Serial && (controlFlag || serialFlag)) {
+  if (!Serial && (controlFlag || serialFlag))
+  {
 
-    while(queue.itemCount() != 0) {
+    while (queue.itemCount() != 0)
+    {
       queue.dequeue();
     }
 
@@ -278,5 +304,4 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(GRIPPER_PIN, LOW);
   }
-  
 }
